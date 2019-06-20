df2977b0261bc1dbcf73b7a26fe5625c1e82a778
Andrey <somabaka@gmail.com>
2019-06-20 19:20:18 +0300

    fix alignment

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,30 +5,18 @@
 </template>
 
 <style>
-html {
-  font-family: "SF UI Display", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-  width: 100%;
-  height: 100%;
-  position: absolute; top: 0; left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background: rgba(237, 36, 0, 1);
-  background: linear-gradient(to bottom, rgba(237, 0, 0, 1) 0%, rgba(224, 26, 0, 1) 32%, rgba(219, 22, 0, 1) 68%, rgba(151, 0, 0, 1) 100%);
-  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#ed2400', endColorstr='#a30e00', GradientType=0);
-}
+  html {
+    background-attachment: fixed;
+    background: rgba(237, 36, 0, 1);
+    background: linear-gradient(to bottom, rgba(151,0,0,1) 0%, rgba(207,0,0,1) 10%, rgba(237,0,0,1) 49%, rgba(207,0,0,1) 90%, rgba(151,0,0,1) 100%);
+  }
 
-*, *:before, *:after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  body {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
 
 </style>
 
