eb8531d48c6fce377c901bb78f10a4761159f990
Andrey <somabaka@gmail.com>
2019-06-21 14:37:46 +0300

    fix background

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,9 +6,9 @@
 
 <style>
   html {
+    background: rgba(237,0,0,1);
+    background: linear-gradient(to bottom, rgba(237,0,0,1) 0%, rgba(237,0,0,1) 57%, rgba(151,0,0,1) 100%);
     background-attachment: fixed;
-    background: rgba(237, 36, 0, 1);
-    background: linear-gradient(to bottom, rgba(151,0,0,1) 0%, rgba(207,0,0,1) 10%, rgba(237,0,0,1) 49%, rgba(207,0,0,1) 90%, rgba(151,0,0,1) 100%);
   }
 
   body {
