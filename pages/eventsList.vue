51a2e317846d6ebe77b2e6a65f38996f4b8ff064
Andrey <somabaka@gmail.com>
2019-06-25 12:56:47 +0300

    event list component

--- pages/eventsList.vue
+++ pages/eventsList.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="eventsList">
     <div class="container">
-      <div class="columns is-centered">
+      <div class="columns is-centered is-marginless">
         <div class="column is-12">
-          <div class="level is-mobile">
+          <div class="level is-mobile" style="margin-bottom: 5px">
             <div class="level-left">
               <!--  non  -->
             </div>
@@ -11,58 +11,33 @@
               <p class="has-text-white">
                 Здравствуйте, {{user}}!
               </p>
-              <span class="icon is-medium fa-lg has-text-white">
-                  <i class="far  fa-user-circle"></i>
+                <span class="icon is-medium fa-lg has-text-white">
+                  <img src="../static/icons/Silhouette.png" style="height: 24px; width: 24px" alt="user">
                 </span>
             </div>
           </div>
-          <h1 class="title is-6 has-text-centered has-text-white">
-            Региональная оперативная комиссия
-          </h1>
-          <p class="is-size-7 has-text-white has-text-weight-bold">Дата: {{date}}</p>
-          <div class="level is-mobile">
-            <div class="level-left">
-              <p class="is-size-7 has-text-white has-text-weight-bold">Время: {{time}}-{{time}}</p>
-            </div>
-            <div class="level-right">
-              <p class="is-size-7 has-text-white has-text-weight-bold">Мероприятие началось</p>
-            </div>
-          </div>
-          <div class="columns is-centered is-marginless eventList">
-            <ul class="events " style="width: 295px;">
-              <li
-                v-for="event in events"
-                :key="event.id"
-                class="column event "
-                style="border: 5px solid lightgrey; border-radius: 5px; background-color: white; margin-bottom: 10px">
-                <h1 class="title is-size-7">{{event.eventName}}</h1>
-                <div class="level is-mobile">
-                  <div class="level-left">
-                    <div>
-                      <p class="is-size-7">Голосование:</p>
-                      <p class="is-size-7">Продолжительность:</p>
-                      <p class="is-size-7">Кол-во вопросов:</p>
-                    </div>
-                  </div>
-                  <div class="level-item">
-                    <div>
-                      <div style="display: flex; align-items: center; height: 18px; width: 18px;">
-                        <div class=""
-                             style="height: 12px; width: 12px; background-color: #20c412; border-radius: 50%"></div>
-                      </div>
-                      <p class="is-size-7">{{event.timer}}</p>
-                      <p class="is-size-7">{{2}}</p>
-                    </div>
-                  </div>
-                  <div class="level-right">
-                    <div>
-                      <p class="is-size-7" style="height: 54px">{{event.status}}</p>
-                    </div>
-                  </div>
+          <div style="margin-bottom: 10px">
+            <div v-if="eventSelected">
+              <h1 class="title is-6 has-text-white" style="margin-bottom: 5px">
+                Региональная оперативная комиссия
+              </h1>
+              <p class="is-size-7-mobile has-text-white has-text-weight-bold">Дата: {{date}}</p>
+              <div class="level is-mobile">
+                <div class="level-left">
+                  <p class="is-size-7-mobile has-text-white has-text-weight-bold">Время: {{time}}-{{time}}</p>
                 </div>
-              </li>
-            </ul>
+                <div class="level-right">
+                  <p class="is-size-7-mobile has-text-white has-text-weight-bold">Мероприятие началось</p>
+                </div>
+              </div>
+            </div>
+            <div class="title" v-else>
+              <h1 class="title is-6 has-text-white">
+                Список мероприятий
+              </h1>
+            </div>
           </div>
+          <events/>
         </div>
       </div>
     </div>
@@ -70,63 +45,20 @@
 </template>
 
 <script>
+  import Events from '../components/events'
+
   export default {
     name: 'eventsList',
+    components: {
+      Events
+    },
     data () {
       return {
         user: 'Иван Иванович',
         id: 0,
         date: '',
         time: '',
-        events: [{
-          id: 1,
-          eventName: '««Эффуктивные меры по продвижению вагонопотока»»',
-          timer: 10 + ' мин',
-          status: 'началось'
-        },
-          {
-            id: 2,
-            eventName: '««Эффуктивные меры по продвижению вагонопотока»»',
-            timer: 10 + ' мин',
-            status: 'началось'
-          },
-          {
-            id: 3,
-            eventName: '««Эффуктивные меры по продвижению вагонопотока»»',
-            timer: 10 + ' мин',
-            status: 'началось'
-          },
-          {
-            id: 4,
-            eventName: '««Эффуктивные меры по продвижению вагонопотока»»',
-            timer: 10 + ' мин',
-            status: 'началось'
-          },
-          {
-            id: 1,
-            eventName: '««Эффуктивные меры по продвижению вагонопотока»»',
-            timer: 10 + ' мин',
-            status: 'началось'
-          },
-          {
-            id: 2,
-            eventName: '««Эффуктивные меры по продвижению вагонопотока»»',
-            timer: 10 + ' мин',
-            status: 'началось'
-          },
-          {
-            id: 3,
-            eventName: '««Эффуктивные меры по продвижению вагонопотока»»',
-            timer: 10 + ' мин',
-            status: 'началось'
-          },
-          {
-            id: 4,
-            eventName: '««Эффуктивные меры по продвижению вагонопотока»»',
-            timer: 10 + ' мин',
-            status: 'началось'
-          },
-        ]
+        eventSelected: false
       }
     },
     mounted () {
